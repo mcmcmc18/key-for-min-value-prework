653e9134a0a38e0434e5641dfcb61deb57d1256e
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-29 20:28:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,14 @@ def key_for_min_value(name_hash)
 name_hash.collect do |key, value|
 name_arr = []
 name_arr.push(value)
-if value < 
+if name_arr[i] < name_arr[i++]
+lowest_value = name_arr[i]
+else
+  lowest_value = name_arr[i++]
+  i++
+  
+name_hash.collect do |key, value|
+  if lowest_value = value
+    return key
+  
 end
